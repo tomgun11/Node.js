42e8ba799976fb130f42e60483ebbf31ff40f5e6
tomgun11 <31482512+tomgun11@users.noreply.github.com>
2017-09-07 15:00:55 +0300

    From Work - Adding notes
    Changed the add not elogic to wotk

--- Node.js/notes-node/Playground/json.js
+++ Node.js/notes-node/Playground/json.js
@@ -7,8 +7,23 @@
 // console.log(stringObj);
 
 
-var personString = '{"name": "Tom", "age": 30}';
+// var personString = '{"name": "Tom", "age": 30}';
 
-var person = JSON.parse(personString);
-console.log(typeof person);
-console.log(person);
+// var person = JSON.parse(personString);
+// console.log(typeof person);
+// console.log(person);
+const fs = require('fs');
+
+var originalNote = {
+    title: 'Some Title',
+    body: 'Content'
+};
+
+var originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteString);
+
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString);
+
+console.log(typeof note);
+console.log(note.title);
--- Node.js/notes-node/notes.js
+++ Node.js/notes-node/notes.js
@@ -1,8 +1,25 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
 var addNote = (title, body) => {
-  console.log('Addning Note', title, body);
-}
+  var notes = [];
+  var note = {
+    title,
+    body
+  };
+
+  if (fs.exists('notes-data.json')){
+    var notesString = fs.readFileSync('notes-data.json');  
+    notes = JSON.parse(notesString);
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  }
+  else{
+    notes.push(note);
+    fs.appendFileSync('notes-data.json', JSON.stringify(notes));
+  }
+};
 
 var getAll = () => {
   console.log('Getting all notes');
